0de2d1df6e6ae6a975102eaeda1bf9b86d086246
Vanna Winland <vgwinland@gmail.com>
2019-09-10 19:37:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts ""
+    puts "NO, NOT SINCE 1938!"
+  else phrase
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
